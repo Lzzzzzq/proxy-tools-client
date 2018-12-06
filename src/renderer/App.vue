e7d9cf7965d283bc7d8a0ae681ffd71d842b331b
lzzzzzq <1441001905@qq.com>
2018-12-06 19:25:40 +0800

    feat: 初始化项目

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'proxy-tools-client'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
