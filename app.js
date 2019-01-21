253c44fc9c8a6d7ff3d38b7e84b38a5b02de3838
lzzzzzq <1441001905@qq.com>
2019-01-21 17:06:30 +0800

    feat: 增加开发环境下命令

--- app.js
+++ app.js
@@ -4,18 +4,6 @@ const portfinder = require('portfinder')
 
 const init = async () => {
   try {
-    // Launch the browser.
-    const app = await carlo.launch({
-      width: 1366,
-      height: 768
-    });
-
-    // Terminate Node.js process on app window closing.
-    app.on('exit', () => process.exit());
-
-    // Tell carlo where your web files are located.
-    app.serveFolder(__dirname);
-
     const port = 3000
 
     portfinder.basePort = port
@@ -25,14 +13,30 @@ const init = async () => {
       console.log(`Port -> ${port}`)
 
       process.env.socketPort = port
-
-      // Expose 'env' function in the web environment.
-      await app.exposeFunction('env', _ => process.env);
       
       socketInit(port)
+      
+      if (process.env.NODE_ENV !== 'development') {
 
-      // Navigate to the main page of your app.
-      await app.load('./view/dist/index.html');
+        // Launch the browser.
+        const app = await carlo.launch({
+          width: 1366,
+          height: 768
+        });
+    
+        // Terminate Node.js process on app window closing.
+        app.on('exit', () => process.exit());
+    
+        // Tell carlo where your web files are located.
+        app.serveFolder(__dirname);
+
+        // Expose 'env' function in the web environment.
+        await app.exposeFunction('env', _ => process.env);
+
+        // Navigate to the main page of your app.
+        await app.load('./view/dist/index.html');
+
+      }
     })
   } catch (e) {
     console.log('Error: ', e)
